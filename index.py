e4f1a03469f71c0efc2ad401e7abea0e4f293fff
Galaxyyyyy <89131534+Galaxyyyyy@users.noreply.github.com>
2023-07-16 13:06:38 +0530

    test

--- index.py
+++ index.py
@@ -3,19 +3,23 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
-#intents
+load_dotenv()
+# intents
 intents = discord.Intents.default()
 intents.message_content = True
 
 # bot setup
-bot = commands.Bot(command_prefix=".", intents = intents, status = discord.Status.idle, activity = discord.Game(name = " with your"))
+bot = commands.Bot(command_prefix=".", intents=intents,
+                   status=discord.Status.idle, activity=discord.Game(name=" with your"))
+
 
 @bot.event
 async def on_ready():
     print("Successfully logged in!")
-    
+
+
 @bot.command()
 async def ping(ctx):
     await ctx.send("I hate niggers")
 
-bot.run(token=os.getEnv("DISCORD_BOT_TOKEN"))
+bot.run(token=os.getenv("DISCORD_BOT_TOKEN"))
